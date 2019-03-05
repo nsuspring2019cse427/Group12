0d3222eba26eb3580d5b06280435ceff89829b95
Abdullah-Abir <abdullah.abir@northsouth.edu>
2019-03-06 03:08:45 +0600

    add test db

--- Project01/Code/burger-wagon/server/server-test.py
+++ Project01/Code/burger-wagon/server/server-test.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 
 from .server import app
 
@@ -11,7 +12,10 @@ class BasicTestCase(unittest.TestCase):
         response = tester.get('/', content_type='application/json; charset=UTF-8')
         self.assertEqual(response.status_code, 404)
 
-    # TODO test database
+    def test_database(self):
+        exists = os.path.exists('burger_wagon.db')
+
+        self.assertTrue(exists)
 
 
 if __name__ == '__main__':
