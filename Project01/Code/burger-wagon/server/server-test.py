e0cda56feb88a3faaa18adc050f9aa16df7a8f1e
Inconnu08 <taufiqrx8@gmail.com>
2019-03-05 17:07:57 +0600

    add server app instance to pass index test

--- Project01/Code/burger-wagon/server/server-test.py
+++ Project01/Code/burger-wagon/server/server-test.py
@@ -1,5 +1,7 @@
 import unittest
 
+from .server import app
+
 
 class BasicTestCase(unittest.TestCase):
 
@@ -7,3 +9,7 @@ class BasicTestCase(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/', content_type='application/json; charset=UTF-8')
         self.assertEqual(response.status_code, 404)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Project01/Code/burger-wagon/server/server.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_restful import Api
+
+# creates an instance of server app
+app = Flask(__name__)
+api = Api(app)
+
+if __name__ == '__main__':
+    app.run()
