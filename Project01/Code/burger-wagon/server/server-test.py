6a69f08320a3df9657a74d8ebed76bbeef6d35a5
Inconnu08 <taufiqrx8@gmail.com>
2019-03-06 00:56:28 +0600

    todo: create test database exists

--- Project01/Code/burger-wagon/server/server-test.py
+++ Project01/Code/burger-wagon/server/server-test.py
@@ -6,9 +6,12 @@ from .server import app
 class BasicTestCase(unittest.TestCase):
 
     def test_index(self):
+        """ Tests index in other words where the server is running fine. """
         tester = app.test_client(self)
         response = tester.get('/', content_type='application/json; charset=UTF-8')
         self.assertEqual(response.status_code, 404)
+
+    # TODO test database
 
 
 if __name__ == '__main__':
--- Project01/Code/burger-wagon/server/server.py
+++ Project01/Code/burger-wagon/server/server.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 
 # creates an instance of server app
 app = Flask(__name__)
+app.config.from_object(__name__)
 api = Api(app)
 
 if __name__ == '__main__':
