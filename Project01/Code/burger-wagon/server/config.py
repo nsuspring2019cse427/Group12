fb1d058c564e37102f9e11d82a3607c3c7f7337b
Inconnu08 <taufiqrx8@gmail.com>
2019-03-06 22:23:24 +0600

    add flask config for testing and developement

---
+++ Project01/Code/burger-wagon/server/config.py
@@ -0,0 +1,48 @@
+# encoding: utf-8
+
+"""
+
+Configuration of Flask for different cases.
+
+"""
+
+# no imports
+import os
+
+
+class BaseConfig(object):
+    # configuration
+
+    DEBUG = False
+    TESTING = False
+
+    SECRET_KEY = 'weneedthemarks'
+    USERNAME = 'admin'
+    PASSWORD = '123'
+
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    DATABASE = 'burger_wagon.db'
+
+    # define the full path for the database
+    DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+    # database config
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    TESTING = True
+
+
+class TestingConfig(BaseConfig):
+    DEBUG = False
+    TESTING = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
+    TESTING = False
+
--- Project01/Code/burger-wagon/server/server-test.py
+++ Project01/Code/burger-wagon/server/server-test.py
@@ -5,16 +5,19 @@ from .server import app
 
 
 class BasicTestCase(unittest.TestCase):
+    """ Basic initial testings. """
 
     def test_index(self):
         """ Tests index in other words where the server is running fine. """
+
         tester = app.test_client(self)
         response = tester.get('/', content_type='application/json; charset=UTF-8')
         self.assertEqual(response.status_code, 404)
 
     def test_database(self):
-        exists = os.path.exists('burger_wagon.db')
+        """ Tests whether database exists. """
 
+        exists = os.path.exists('burger_wagon.db')
         self.assertTrue(exists)
 
 
