8d73b780ef092b046825b7fc71baacf1b68bff7b
Inconnu08 <taufiqrx8@gmail.com>
2019-03-15 02:19:26 +0600

    update function create_app to pass test

--- Project01/Code/burger-wagon/app/__init__.py
+++ Project01/Code/burger-wagon/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_restplus import Api
 from flask_sqlalchemy import SQLAlchemy
 
 # initialize sql-alchemy before app creation
@@ -18,5 +19,7 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     db.init_app(app)
+    api = Api(app, version='1.0', title='Burger Wagon', description='api endpoints')
+    api.namespace(name='', description='menu of all the items available and CRUD operations')
 
     return app
