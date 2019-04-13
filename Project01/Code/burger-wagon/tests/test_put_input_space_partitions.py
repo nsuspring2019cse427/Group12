20a5af0758835c3621e385a467eaaba32c3fe065
Inconnu08 <taufiqrx8@gmail.com>
2019-04-13 18:10:52 +0600

    add initial partition unit tests for put methods

--- Project01/Code/burger-wagon/tests/test_put_input_space_partitions.py
+++ Project01/Code/burger-wagon/tests/test_put_input_space_partitions.py
@@ -5,7 +5,7 @@ from app import create_app, db
 
 
 class PostInputSpaceTestCase(unittest.TestCase):
-    """ This class represents testing input space partitioning of all POST endpoints. """
+    """ This class represents testing input space partitioning of all PUT endpoints. """
 
     # @before fixture
     def setUp(self):
@@ -15,7 +15,13 @@ class PostInputSpaceTestCase(unittest.TestCase):
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client
 
-        # fixture/ data-points : dummy used for POST endpoints
+        # fixture/ data-points : dummy used for POST/PUT endpoints
+        self.menu_item_ok = json.dumps({
+            "title": "Cheese lava burger",
+            "description": "Cheese and red sauce molten like lava.",
+            "price": 1202.00
+        })
+
         self.menu_item_numerical = json.dumps({
             "title": "57983479",
             "description": "pet kharap hobe na 50%",
@@ -33,17 +39,23 @@ class PostInputSpaceTestCase(unittest.TestCase):
             # create all tables of database
             db.create_all()
 
-    def test_menu_item_creation_should_not_accept_empty_title(self):
-        """ - Test API should not create a menu item with empty title (POST request). """
+    def test_menu_item_edit_should_not_accept_empty_title(self):
+        """ - Test API should not successfully edit a menu item with empty title (PUT request). """
 
-        res = self.client().post('/menu', data=self.menu_item_numerical, content_type='application/json')
+        res = self.client().post('/menu', data=self.menu_item_ok, content_type='application/json')
+        self.assertEqual(res.status_code, 201)
+
+        res = self.client().put('/menu/1', data=self.menu_item_numerical, content_type='application/json')
         self.assertEqual(res.status_code, 400)
         self.assertIn('food title can only be alphabets', str(res.data))
 
-    def test_menu_item_creation_should_not_accept_numerical_title(self):
-        """ - Test API should not create a menu item with numerical title value (POST request). """
+    def test_menu_edit_creation_should_not_accept_numerical_title(self):
+        """ - Test API should not successfully edit a menu item with numerical title value (PUT request). """
 
-        res = self.client().post('/menu', data=self.menu_item_numerical, content_type='application/json')
+        res = self.client().post('/menu', data=self.menu_item_ok, content_type='application/json')
+        self.assertEqual(res.status_code, 201)
+
+        res = self.client().put('/menu/1', data=self.menu_item_numerical, content_type='application/json')
         self.assertEqual(res.status_code, 400)
         self.assertIn('food title can only be alphabets', str(res.data))
 
