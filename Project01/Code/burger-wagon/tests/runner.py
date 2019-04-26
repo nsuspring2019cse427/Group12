22a19b9cba084d1f725f2303238d0844e436e1e0
Inconnu08 <taufiqrx8@gmail.com>
2019-04-26 19:56:51 +0600

    remove unnecessary integration test

--- Project01/Code/burger-wagon/tests/runner.py
+++ Project01/Code/burger-wagon/tests/runner.py
@@ -1,6 +1,8 @@
 import os
 import unittest
 
+""" This is a test suite that tests integration of all modules and tests. Automates the boring stuffs. """
+
 loader = unittest.TestLoader()
 tests_dir = os.path.abspath(os.path.dirname(__file__))
 suite = loader.discover(tests_dir)
